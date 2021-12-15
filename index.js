177bc8f403ba626b80330183ff1a03e9046cb6cd
Jabal Surya Ngalam <87789231+Jabalsurya2105@users.noreply.github.com>
2021-12-15 21:11:53 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -57,14 +57,14 @@ for (let num of participants) {
 try {
 ppuser = await hisoka.profilePictureUrl(num, 'image')
 } catch {
-ppuser = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+ppuser = 'https://i.ibb.co/t2m9fFt/88876ba5bb74.jpg'
 }
 
 // Get Profile Picture Group
 try {
 ppgroup = await hisoka.profilePictureUrl(anu.id, 'image')
 } catch {
-ppgroup = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+ppgroup = 'https://i.ibb.co/t2m9fFt/88876ba5bb74.jpg'
 }
 
 if (anu.action == 'add') {
